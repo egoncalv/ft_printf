98b1b8b324900f08a738e2b610708325a3ebe804
Erick Gonçalves <erickbarros06@hotmail.com>
2022-03-28 21:41:45 +0100

    dealing with hexadecimals

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 int main ()
 {
-	ft_printf("This is mine:       %x", 123213);
-	printf("\nThis is printf:    %x", 123213);
+	ft_printf("This is mine:       %x", 0);
+	printf("\nThis is printf:    %x", 0);
 	return (0);
 }
 
--- src/ft_treat_hex.c
+++ src/ft_treat_hex.c
@@ -15,28 +15,24 @@
 int	ft_convert_hex(unsigned int nbr, char format)
 {
 	static int	count;
-	if (nbr == 0)
+
+	if (nbr >= 16)
 	{
-		write(1, "0", 1);
-		return (1);
-	}
- 	if (nbr >= 16)
-	{
-		count++;
+		count ++;
 		ft_convert_hex(nbr / 16, format);
 		ft_convert_hex(nbr % 16, format);
 	}
 	else
 	{
-		if (nbr <= 9)
-			ft_putchar_fd((nbr + '0'), 1);
-		else
+		if (nbr >= 10)
 		{
 			if (format == 'x')
-				ft_putchar_fd((nbr - 10 + 'a'), 1);
-			if (format == 'X')
-				ft_putchar_fd((nbr - 10 + 'A'), 1);
+				ft_putchar_fd(nbr + 87, 1);
+			else if (format == 'X')
+				ft_putchar_fd(nbr + 55, 1);
 		}
+		else
+			ft_putchar_fd(nbr + 48, 1);
 	}
 	return (count + 1);
 }
